ce1108b256f9049996476ebca72f30c81e1b48b6
Yobih <160126805+254100934193@users.noreply.github.com>
2024-09-01 10:33:54 +0300

    MUNGAI

--- Commandes by Yobih/kingCarl.js
+++ Commandes by Yobih/kingCarl.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "william", reaction: "📷", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'CARL-HD-PHOTO \n\n ' + "*ᴄᴀʀʟ-ᴛᴇᴄʜ.*";
+    let z = 'YOBIH MD \n\n ' + "*MUNGAI.*";
     let d = ' #KING'S';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/10293b39bc439c0abc513.jpg';
+    var img = 'https://telegra.ph/file/d0cd3c82fbbc120f38ac4.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -22,6 +22,6 @@ async function  execute  (origineMessage,zok) {
    let z ='Salut je m\'appelle *FLASH-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *France King*'
       let varmess=z+d
-      var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
+      var img='https://telegra.ph/file/d0cd3c82fbbc120f38ac4.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
