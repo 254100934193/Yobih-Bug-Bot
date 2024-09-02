8fb42a326ca557b7ac2fbe9cba308cb1eaa22e58
Yobih <160126805+254100934193@users.noreply.github.com>
2024-09-02 21:34:59 +0300

    MUNGAI

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -1,77 +1,45 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const axios = require("axios");
-const { Sticker, StickerTypes } = require("wa-sticker-formatter");
-const {zokou} = require("../framework/zokou");
-
-zokou({
-  nomCom: "stickersearch",
-  categorie: 'Search',
-  reaction: "🌫"
-},
-async (dest, zk, commandeOptions) => {
-  const { repondre, ms, arg, nomAuteurMessage } = commandeOptions;
-
-  if (!arg[0]) {
-    repondre("where is the request ? !");
-    return;
-  }
-
-  const gifSearchTerm = arg.join(" ");
-  const tenorApiKey = "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c"; // Remplacez par votre clé d'API Tenor
-
-  try { for ( i = 0 ; i < 5 ; i++) {
-    const gif = await axios.get(
-      `https://tenor.googleapis.com/v2/search?q=${gifSearchTerm}&key=${tenorApiKey}&client_key=my_project&limit=8&media_filter=gif`
-    );
-
-    const gifUrl = gif.data.results[i].media_formats.gif.url;
-
-    
-   
-
-    // Assurez-vous de remplacer les valeurs manquantes dans la création du sticker
-    const packname = nomAuteurMessage; // Remplacez par le nom de votre pack de stickers
-
-    const stickerMess = new Sticker(gifUrl, {
-      pack: MUNGAI,
-      author: 'MUNGAI YOBIH',
-      type: StickerTypes.FULL,
-      categories: ["🤩", "🎉"],
-      id: "12345",
-      quality: 60,
-      background: "transparent",
-    });
-    const stickerBuffer2 = await stickerMess.toBuffer();
-    zk.sendMessage(dest, { sticker: stickerBuffer2 }, { quoted: ms }); }
-  } catch (error) {
-    console.error("Erreur lors de la recherche de stickers :", error);
-    repondre("Erreur lors de la recherche de stickers.");
-  }
-});
+const {
+    smd,
+    tlang,
+    prefix,
+    Config,
+    sleep,
+    astroJson,
+    smdBuffer
+  } = require("../lib");
+  const axios = require("axios");
+  smd({
+    cmdname: "stickersearch",
+    alias: ["sticsearch"],
+    category: "search",
+    use: "[text]",
+    info: "Searches Stickers"
+  }, async (_0x4cc234, _0xa151c7) => {
+    try {
+      const {
+        generateSticker: _0x5a889c
+      } = require("../lib");
+      if (!_0xa151c7) {
+        return _0x4cc234.reply("Sorry you did not give any search term!");
+      }
+      const _0x1f4c84 = await axios.get("https://g.tenor.com/v1/search?q=" + _0xa151c7 + "&key=LIVDSRZULELA&limit=8").catch(() => {});
+      if (!_0x1f4c84.data || !_0x1f4c84.data.results || !_0x1f4c84.data.results[0]) {
+        return _0x4cc234.reply("*Could not find!*");
+      }
+      let _0x43fcbc = _0x1f4c84.data.results.length > 5 ? 5 : _0x1f4c84.data.results.length;
+      for (let _0x48da09 = 0; _0x48da09 < _0x43fcbc; _0x48da09++) {
+        let _0x2a4632 = await smdBuffer(_0x1f4c84.data.results?.[_0x48da09]?.media[0]?.mp4?.url);
+        let _0x1c1454 = {
+          pack: Config.packname,
+          author: Config.author,
+          type: "full",
+          quality: 1
+        };
+        if (_0x2a4632) {
+          _0x5a889c(_0x4cc234, _0x2a4632, _0x1c1454);
+        }
+      }
+    } catch (_0x3f900e) {
+      _0x4cc234.error(_0x3f900e + "\n\nCommand: stickersearch", _0x3f900e, "*Could not find*");
+    }
+  });
