e580626959a94bff6b75bd37aac0e62dcd40c94a
Yobih <160126805+254100934193@users.noreply.github.com>
2024-09-12 21:58:56 +0300

    MUNGAI

--- commandes/Quran-surah.js
+++ commandes/Quran-surah.js
@@ -1,3 +1,11 @@
+const { zokou } = require("../framework/zokou");
+const axios = require("axios");
+const Genius = require("genius-lyrics");
+const Client = new Genius.Client("jKTbbU-6X2B9yWWl-KOm7Mh3_Z6hQsgE4mmvwV3P3Qe7oNa9-hsrLxQV5l5FiAZO");
+
+
+
+
 import fetch from 'node-fetch';
 import { translate } from '@vitalets/google-translate-api';
 
