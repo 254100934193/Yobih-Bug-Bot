ddfadad63cacfad6925e3450def37d5c2b2f2ec7
Yobih <160126805+254100934193@users.noreply.github.com>
2024-09-14 07:35:11 +0300

    MUNGAI

--- commandes/galaxy.js
+++ commandes/galaxy.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "galaxy", reaction: "🌌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝗛𝗘𝗟𝗟𝗢 👋  \n\n ' + "𝗔𝗠 𝗖𝗬𝗕𝗘𝗥𝗜𝗢𝗡-𝗩1 𝗕𝗬 𝗖𝗔𝗥𝗟𝗧𝗘𝗖𝗛 𝗔𝗡𝗗 𝗦𝗧𝗜𝗟𝗟 𝗘𝗫𝗣𝗟𝗢𝗥𝗜𝗡𝗚 𝗧𝗛𝗘 𝗚𝗔𝗟𝗔𝗫𝗬";
+    let z = '𝗛𝗘𝗟𝗟𝗢 👋  \n\n ' + "AM YOBIH BUG BOT BY MUNGAI AND I'M STILL EXPLORING THE GALAXY OF MUNGAI";
     let d = 'The galaxy is so favourable, wish you could be here';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/51b372ed62c6d83eef249.jpg';
@@ -19,7 +19,7 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *FLASH-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+   let z ='Salut je m\'appelle *YOBIH-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
       let d =' developpé par *France King*'
       let varmess=z+d
       var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
