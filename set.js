bf6b43533687752044689cade682af4baac0ddf0
Yobih <160126805+254100934193@users.noreply.github.com>
2024-12-25 20:19:47 +0300

    Update set.js

--- set.js
+++ set.js
@@ -1,39 +1,42 @@
+
+
+
 const fs = require('fs-extra');
-if (fs.existsSync("set.env")) {
-  require("dotenv").config({
-    'path': __dirname + '/set.env'
-  });
-}
-const path = require('path');
-const databasePath = path.join(__dirname, "./database.db");
-const DATABASE_URL = process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL;
-module.exports = {
-  'session': process.env.SESSION_ID || "mungai",
-  'PREFIXE': process.env.PREFIX || '~',
-  'OWNER_NAME': process.env.OWNER_NAME || "mungai",
-  'NUMERO_OWNER': process.env.NUMERO_OWNER || 'yobih',
-  'AUTO_READ_STATUS': process.env.AUTO_READ_STATUS || "non",
-  'AUTO_DOWNLOAD_STATUS': process.env.AUTO_DOWNLOAD_STATUS || "non",
-  'BOT': process.env.BOT_NAME || "Yobih_MD",
-  'URL': process.env.BOT_MENU_LINKS || "https://telegra.ph/file/d0cd3c82fbbc120f38ac4.jpg",
-  'MODE': process.env.PUBLIC_MODE || "yes",
-  'PM_PERMIT': process.env.PM_PERMIT || 'no',
-  'HEROKU_APP_NAME': process.env.HEROKU_APP_NAME || null,
-  'HEROKU_APY_KEY': process.env.HEROKU_APY_KEY || null,
-  'WARN_COUNT': process.env.WARN_COUNT || '3',
-  'ETAT': process.env.PRESENCE || '',
-  'DP': process.env.STARTING_BOT_MESSAGE || 'yes',
-  'CHATBOT': process.env.PM_CHATBOT || 'no',
-  'HEROKU': process.env.HEROKU || "yes",
-  'ANTI_VV': process.env.ANTI_VIEW_ONCE || 'no',
-  'ANTI_CMD_SPAM': process.env.ANTI_COMMAND_SPAM || 'no',
-  'DATABASE_URL': DATABASE_URL,
-  'DATABASE': DATABASE_URL === databasePath ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9"
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || '',
+    PREFIXE: process.env.PREFIX || ".",
+    OWNER_NAME: process.env.OWNER_NAME || "Yobih md",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " Yobih-MD",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
+    BOT : process.env.BOT_NAME || 'Yobih md bug Bot',
+    URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/d0cd3c82fbbc120f38ac4.jpg',
+    MODE: process.env.PUBLIC_MODE || "yes",
+    PM_PERMIT: process.env.PM_PERMIT || 'yes',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    ETAT : process.env.PRESENCE || '',
+    CHATBOT : process.env.PM_CHATBOT || 'no',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
+   
 };
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
-  fs.unwatchFile(fichier);
-  console.log("mise à jour " + __filename);
-  delete require.cache[fichier];
-  require(fichier);
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
 });
+
