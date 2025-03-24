dd4afe4f9d86773b50c782bfa5a84f9b753f393d
Yobih <160126805+254100934193@users.noreply.github.com>
2025-03-24 09:08:59 +0300

    mungai mkuu8

--- commandes/youtube.js
+++ commandes/youtube.js
@@ -3,7 +3,7 @@ const {
 } = require('../framework/zokou');
 const yts = require('yt-search');
 zokou({
-  'nomCom': "vide",
+  'nomCom': "video",
   'categorie': "Search",
   'reaction': '👀'
 }, async (_0x3a6027, _0x19584d, _0x6cc5b8) => {
@@ -56,7 +56,7 @@ zokou({
   }
 });
 zokou({
-  'nomCom': "playi",
+  'nomCom': "play",
   'categorie': 'Download',
   'reaction': '🎧'
 }, async (_0x5168b7, _0x1d6d3f, _0x3b9b4d) => {
@@ -109,7 +109,7 @@ zokou({
   }
 });
 zokou({
-  'nomCom': "song1",
+  'nomCom': "song",
   'categorie': "Download",
   'reaction': '🎸'
 }, async (_0x202967, _0x1761c4, _0x1a3bdd) => {
